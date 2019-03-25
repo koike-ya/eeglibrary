7dcb544adcb8799243f8b1dd41be6e6aa24f2bf8
Tomoya Koike <makeffort134@gmail.com>
2019-03-25 10:37:15 +0900

    sampler available

--- eeglibrary/src/eeg_dataloader.py
+++ eeglibrary/src/eeg_dataloader.py
@@ -1,4 +1,6 @@
 from torch.utils.data import DataLoader
+import torch
+import numpy as np
 
 
 class EEGDataLoader(DataLoader):
@@ -6,3 +8,12 @@ class EEGDataLoader(DataLoader):
         super(EEGDataLoader, self).__init__(*args, **kwargs)
     #
     # def collate_fn(self, batch):
+
+
+def make_weights_for_balanced_classes(labels, n_classes):
+    labels = np.array(labels, dtype=int)
+    class_count = [np.sum(labels == class_index) for class_index in range(n_classes)]
+    weight_per_class = 1 / torch.Tensor(class_count)
+    # weights = [weight_per_class[label] for label in labels]
+    return weight_per_class.double()
+
--- eeglibrary/src/eeg_dataset.py
+++ eeglibrary/src/eeg_dataset.py
@@ -3,24 +3,34 @@ from eeglibrary.src.eeg_parser import EEGParser
 
 
 class EEGDataSet(Dataset, EEGParser):
-    def __init__(self, manifest_filepath, labels, eeg_conf, normalize=False, augment=False):
+    def __init__(self, manifest_filepath, eeg_conf, classes=None, normalize=False, augment=False):
         with open(manifest_filepath, 'r') as f:
             path_list = f.readlines()
         path_list = [p.strip() for p in path_list]
 
         self.path_list = path_list
         self.size = len(path_list)
-        self.labels = labels
+        self.classes = classes # self.classes is None in test dataset
+        self.suffix = path_list[0][-4:]
         super(EEGDataSet, self).__init__(eeg_conf, normalize, augment)
 
     def __getitem__(self, index):
         eeg_path = self.path_list[index]
         y = self.parse_eeg(eeg_path)
-        if eeg_path[-4:] == '.pkl':
-            label = eeg_path.split('/')[-2].split('_')[2]
+        if self.classes:
+            label = self._parse_label(eeg_path)
+            return y, self.classes.index(label)
         else:
-            label = eeg_path.split('_')[2]
-        return y, self.labels.index(label)
+            return y
 
     def __len__(self):
         return self.size
+
+    def _parse_label(self, path):
+        if self.suffix == '.pkl':
+            return path.split('/')[-2].split('_')[2]
+        else:
+            return path.split('_')[2]
+
+    def labels_index(self) -> [int]:
+        return [self.classes.index(self._parse_label(path)) for path in self.path_list]
