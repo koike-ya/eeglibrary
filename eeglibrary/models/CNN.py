10e761b3aff992872e6bc92c1aac0956ce756851
Tomoya Koike <makeffort134@gmail.com>
2019-03-26 13:00:38 +0900

    3d cnn available

--- eeglibrary/models/CNN.py
+++ eeglibrary/models/CNN.py
@@ -27,7 +27,7 @@ class vgg_16:
 
 
 class CNN(nn.Module):
-    def __init__(self, features, in_features, n_labels=2):
+    def __init__(self, features, in_features, n_labels=2, dim=2):
         super(CNN, self).__init__()
         self.features = features
         self.classifier = nn.Sequential(
@@ -40,8 +40,11 @@ class CNN(nn.Module):
             nn.Linear(4096, n_labels),
         )
         self.softmax = nn.Softmax(dim=-1)
+        self.dim = dim
 
     def forward(self, x):
+        if self.dim == 3:
+            x = torch.unsqueeze(x, dim=1)
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
@@ -49,14 +52,19 @@ class CNN(nn.Module):
         return x
 
 
-def make_layers(cfg, in_channel=1):
+def make_layers(cfg, in_channel=1, dim='2'):
+    if dim == 2:
+        conv_cls, max_pool_cls, batch_norm_cls = nn.Conv2d, nn.MaxPool2d, nn.BatchNorm2d
+    elif dim == 3:
+        conv_cls, max_pool_cls, batch_norm_cls = nn.Conv3d, nn.MaxPool3d, nn.BatchNorm3d
+
     layers = []
     for i, (channel, kernel_size, stride, padding) in enumerate(cfg):
         if channel == 'M':
-            layers += [nn.MaxPool2d(kernel_size=kernel_size, stride=stride, padding=padding)]
+            layers += [max_pool_cls(kernel_size=kernel_size, stride=stride, padding=padding)]
         else:
-            conv2d = nn.Conv2d(in_channel, channel, kernel_size=kernel_size, stride=stride, padding=padding)
-            layers += [conv2d, nn.BatchNorm2d(channel), nn.ReLU(inplace=True)]
+            conv = conv_cls(in_channel, channel, kernel_size=kernel_size, stride=stride, padding=padding)
+            layers += [conv, batch_norm_cls(channel), nn.ReLU(inplace=True)]
         in_channel = channel
     return nn.Sequential(*layers)
 
@@ -88,3 +96,13 @@ def cnn_ftrs_16_751_751(n_labels=2):
     ]
     out_ftrs = 64 * 12 * 7
     return make_layers(cfg, in_channel=16), out_ftrs
+
+
+def cnn_1_16_751_751(n_labels=1):
+    cfg = [(16, (4, 4, 4), (2, 3, 3), (2, 0, 0)),
+           (32, (4, 4, 4), (2, 3, 3), (2, 2, 2)),
+           (64, (4, 4, 4), (2, 3, 3), (2, 1, 1)),
+           (128, (2, 4, 4), (1, 3, 3), (0, 2, 2))]
+    model = CNN(make_layers(cfg, in_channel=1, dim=3), in_features=256 * 10 * 10, n_labels=n_labels, dim=3)
+
+    return model
