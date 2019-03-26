abf8e5a6b766732f64d2f895236f765745675aeb
Tomoya Koike <makeffort134@gmail.com>
2019-03-26 17:46:31 +0900

    random sampler bug fix

--- eeglibrary/src/eeg_dataloader.py
+++ eeglibrary/src/eeg_dataloader.py
@@ -13,7 +13,7 @@ class EEGDataLoader(DataLoader):
 def make_weights_for_balanced_classes(labels, n_classes):
     labels = np.array(labels, dtype=int)
     class_count = [np.sum(labels == class_index) for class_index in range(n_classes)]
-    weight_per_class = 1 / torch.Tensor(class_count)
-    # weights = [weight_per_class[label] for label in labels]
-    return weight_per_class.double()
+    weight_per_class = sum(class_count) / torch.Tensor(class_count)
+    weights = [weight_per_class[label] for label in labels]
+    return weights
 
