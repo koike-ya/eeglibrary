daacb8db65fe0b85fcfb6104fd434e1a27aed865
Tomoya Koike <makeffort134@gmail.com>
2019-03-30 21:26:29 +0900

    tensorboard available

--- eeglibrary/src/__init__.py
+++ eeglibrary/src/__init__.py
@@ -3,3 +3,4 @@ from eeglibrary.src.eeg_dataloader import *
 from eeglibrary.src.eeg_dataset import *
 from eeglibrary.src.eeg_parser import *
 from eeglibrary.src.eeg_loader import *
+from eeglibrary.src.logger import *
